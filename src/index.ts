b7c83c71ca59fd7f24781bf72216b93a21c29b29
= <=>
2026-03-02 17:58:56 +0530

    feat: Set up initial server infrastructure with Prisma ORM, authentication, comprehensive error handling, and Winston logging, while updating TypeScript configuration and removing an old Prisma config.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,44 @@
-const args = process.argv.slice(2);
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import errorHandler from "./middleware/error/errorHandler";
+import logger from "./utils/logger";
 
-let value = args[0] || "Omkar";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/authRoutes";
+import dotenv from "dotenv";
 
-console.log(`Hello ${value} 👋`);
+dotenv.config();
+const app = express();
+
+// Request logging middleware
+const stream = {
+  write: (message: string) => logger.http(message.trim()),
+};
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms", {
+    stream,
+  }),
+);
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3001"], // Adjust to your client URL
+    credentials: true,
+  }),
+);
+
+app.use("/api/auth", authRoutes);
+
+app.get("/health", (req, res) => {
+  res.send("OK");
+});
+
+// Global Error Handler (Must be LAST)
+app.use(errorHandler);
+
+app.listen(3000, () => {
+  logger.info("Server is running on port 3000");
+});
